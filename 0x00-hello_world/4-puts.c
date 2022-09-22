220741f147bff19318359569475db84a07b0b559
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-09-22 10:47:56 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
  *
- *  *main - entry point
+	*main - entry point
  *
- *   *
- *
- *    *Return: 0 after executing the command
- *
- *     */
-
+     *Return: 0 after executing the command
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
