a24e4b5ed3ddd8cf64f020055ccad0765120ff57
Erica-Livia <e.ingabire1@alustudent.com>
2022-09-22 01:51:51 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
    *main - entry point
+  *
    *Return: 0 after executing the command  
    */
 
-=======
- *
-	*main - entry point
- *
-     *Return: 0 after executing the command
- */
->>>>>>> 220741f147bff19318359569475db84a07b0b559
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
