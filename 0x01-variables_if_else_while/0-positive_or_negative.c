19c359c34418e86e38c65c47b4b101cf746c5acc
Erica-Livia <e.ingabire1@alustudent.com>
2022-09-22 04:23:26 -0700

    P or N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- 
+
+/** 
  * main - Entry point
  * Return: Always 0 (Successs/correct)
  */
 
 int main(void)
-
 {
-
 	int n;
 
 	srand(time(0));
@@ -36,5 +34,4 @@ int main(void)
 	}
 
 	return (0);
-
 }
