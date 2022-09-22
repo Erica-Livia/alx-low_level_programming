1035a6b303db0d88197569fc66357721b0bcc717
Erica-Livia <e.ingabire1@alustudent.com>
2022-09-22 01:59:34 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/** 
+ * main entry point
+ * 
+ * return 0 at the end of the compilation
+ */
+int main(void)
+{
+	printf("\nwith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
