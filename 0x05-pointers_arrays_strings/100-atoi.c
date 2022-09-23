add140c97caa82b3c982f79fec80a1b66aca47c6
Erica-Livia <e.ingabire1@alustudent.com>
2022-09-22 23:36:59 -0700

    hello

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
  *
  *       Return: integer.
  *
- *        */
+ */
 
 int _atoi(char *s)
 
@@ -16,34 +16,23 @@ int _atoi(char *s)
 
 		unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-
-
-			while (*(s + count) != '\0')
+		while (*(s + count) != '\0')
 
 					{
-
-								if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 
 												break;
-
-
-
-										if (*(s + count) == '-')
+			if (*(s + count) == '-')
 
 														pn *= -1;
 
-												
-
-										if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+			if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 
 													{
 
-																	if (size > 0)
+					if (size > 0)
 
 																						m *= 10;
-
-																			
-
 																		size++;
 
 																				}
@@ -53,19 +42,11 @@ int _atoi(char *s)
 													}
 
 
-
-				for (i = count - size; i < count; i++)
-
-						{
-
+				for (i = count - size; i < count; i++)						{
 									oi = oi + ((*(s + i) - 48) * m);
-
-											m /= 10;
-
-												}
-
+	m /= 10;
+			}
 					return (oi * pn);
-
 }
 
 
