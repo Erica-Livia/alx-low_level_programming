4ab95b50b1e946f134b6b762f08a2997739987d8
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-09-23 08:52:48 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,20 @@
-VimCrypt~03!³þ°ÔŒúû©g`ÙS}8;Sª¬_»€u§wD½Úº×ßtã~DæD%«Û‹ªãmæ	*€ˆÍÎï‘ËT‡Íg8o*ýÄñá‹lî°ÈÁ0¯þÑ•‰I7Y-§uä€²{P‡mV€bY§Ö}$÷Flüdœ5…bÖ~TGŠo½ÖúBÓÅÞV0Þœv™xV¥€F·UŽ§Ž½î‹6÷ Úêæ2SL5±h”¹‹¸åÆ~ŒÒèîU¶é‚8ýZÄEŠ3ex(ƒ^æxº›QÞO˜¢ë9	RÑÓ¨ˆ×#(2_¶
-åº4­d6Ñch_¨Û
-ŒÕß›‡ÿ³9Wà2´ý½¶±A0¦Ã•Š{ÜC¾EÅÌÚÙvOÏ[²Úþêó<ü¶ƒ3ˆœY)ß‡7f'¶ÇŽáB G]
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
